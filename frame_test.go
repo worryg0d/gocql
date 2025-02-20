apache/cassandra-gocql-driver

Support for Native Protocol 5 release version

frame_test.go
@@ -26,6 +26,10 @@ package gocql
 
 import (
 	"bytes"
+	"encoding/binary"
+	"errors"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
 	"os"
 	"testing"
 )
@@ -125,5 +129,317 @@ func TestFrameReadTooLong(t *testing.T) {
 	}
 	if head.op != opReady {
 		t.Fatalf("expected to get header %v got %v", opReady, head.op)
+	}
+}
+
+func Test_framer_writeExecuteFrame(t *testing.T) {
+	framer := newFramer(nil, protoVersion5)
+	frame := writeExecuteFrame{
+		preparedID:       []byte{1, 2, 3},
+		resultMetadataID: []byte{4, 5, 6},
+		customPayload: map[string][]byte{
+			"key1": []byte("value1"),
+		},
+		params: queryParams{
+			nowInSeconds:      true,
+			nowInSecondsValue: 123,
+			keyspace:          "test_keyspace",
+		},
+	}
+
+	err := framer.writeExecuteFrame(123, frame.preparedID, frame.resultMetadataID, &frame.params, &frame.customPayload)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	// skipping header
+	framer.buf = framer.buf[9:]
+
+	assertDeepEqual(t, "customPayload", frame.customPayload, framer.readBytesMap())
+	assertDeepEqual(t, "preparedID", frame.preparedID, framer.readShortBytes())
+	assertDeepEqual(t, "resultMetadataID", frame.resultMetadataID, framer.readShortBytes())
+	assertDeepEqual(t, "constistency", frame.params.consistency, Consistency(framer.readShort()))
+
+	flags := framer.readInt()
+	if flags&int(flagWithNowInSeconds) != int(flagWithNowInSeconds) {
+		t.Fatal("expected flagNowInSeconds to be set, but it is not")
+	}
+
+	if flags&int(flagWithKeyspace) != int(flagWithKeyspace) {
+		t.Fatal("expected flagWithKeyspace to be set, but it is not")
+	}
+
+	assertDeepEqual(t, "keyspace", frame.params.keyspace, framer.readString())
+	assertDeepEqual(t, "nowInSeconds", frame.params.nowInSecondsValue, framer.readInt())
+}
+
+func Test_framer_writeBatchFrame(t *testing.T) {
+	framer := newFramer(nil, protoVersion5)
+	frame := writeBatchFrame{
+		customPayload: map[string][]byte{
+			"key1": []byte("value1"),
+		},
+		nowInSeconds:      true,
+		nowInSecondsValue: 123,
+	}
+
+	err := framer.writeBatchFrame(123, &frame, frame.customPayload)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	// skipping header
+	framer.buf = framer.buf[9:]
+
+	assertDeepEqual(t, "customPayload", frame.customPayload, framer.readBytesMap())
+	assertDeepEqual(t, "typ", frame.typ, BatchType(framer.readByte()))
+	assertDeepEqual(t, "len(statements)", len(frame.statements), int(framer.readShort()))
+	assertDeepEqual(t, "consistency", frame.consistency, Consistency(framer.readShort()))
+
+	flags := framer.readInt()
+	if flags&int(flagWithNowInSeconds) != int(flagWithNowInSeconds) {
+		t.Fatal("expected flagNowInSeconds to be set, but it is not")
+	}
+
+	assertDeepEqual(t, "nowInSeconds", frame.nowInSecondsValue, framer.readInt())
+}
+
+type testMockedCompressor struct {
+	// this is an error its methods should return
+	expectedError error
+
+	// invalidateDecodedDataLength allows to simulate data decoding invalidation
+	invalidateDecodedDataLength bool
+}
+
+func (m testMockedCompressor) Name() string {
+	return "testMockedCompressor"
+}
+
+func (m testMockedCompressor) Encode(data []byte) ([]byte, error) {
+	encoded := make([]byte, len(data)+4)
+	binary.BigEndian.PutUint32(encoded, uint32(len(data)))
+	copy(encoded[4:], data)
+	if m.expectedError != nil {
+		return nil, m.expectedError
+	}
+	return encoded, nil
+}
+
+func (m testMockedCompressor) Decode(data []byte) ([]byte, error) {
+	if m.expectedError != nil {
+		return nil, m.expectedError
+	}
+	return data, nil
+}
+
+func (m testMockedCompressor) DecodeSized(data []byte, size uint32) ([]byte, error) {
+	if m.expectedError != nil {
+		return nil, m.expectedError
+	}
+
+	// simulating invalid size of decoded data
+	if m.invalidateDecodedDataLength {
+		return data[:size-1], nil
+	}
+
+	return data, nil
+}
+
+func Test_readUncompressedFrame(t *testing.T) {
+	tests := []struct {
+		name        string
+		modifyFrame func([]byte) []byte
+		expectedErr string
+	}{
+		{
+			name: "header crc24 mismatch",
+			modifyFrame: func(frame []byte) []byte {
+				// simulating some crc invalidation
+				frame[0] = 255
+				return frame
+			},
+			expectedErr: "gocql: crc24 mismatch in frame header",
+		},
+		{
+			name: "body crc32 mismatch",
+			modifyFrame: func(frame []byte) []byte {
+				// simulating body crc32 mismatch
+				frame[len(frame)-1] = 255
+				return frame
+			},
+			expectedErr: "gocql: payload crc32 mismatch",
+		},
+		{
+			name: "invalid frame length",
+			modifyFrame: func(frame []byte) []byte {
+				// simulating body length invalidation
+				frame = frame[:7]
+				return frame
+			},
+			expectedErr: "gocql: failed to read uncompressed frame payload",
+		},
+		{
+			name: "cannot read body checksum",
+			modifyFrame: func(frame []byte) []byte {
+				// simulating body length invalidation
+				frame = frame[:len(frame)-4]
+				return frame
+			},
+			expectedErr: "gocql: failed to read payload crc32",
+		},
+		{
+			name:        "success",
+			modifyFrame: nil,
+			expectedErr: "",
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			framer := newFramer(nil, protoVersion5)
+			req := writeQueryFrame{
+				statement: "SELECT * FROM system.local",
+				params: queryParams{
+					consistency: Quorum,
+					keyspace:    "gocql_test",
+				},
+			}
+
+			err := req.buildFrame(framer, 128)
+			require.NoError(t, err)
+
+			frame, err := newUncompressedFrame(framer.buf, true)
+			require.NoError(t, err)
+
+			if tt.modifyFrame != nil {
+				frame = tt.modifyFrame(frame)
+			}
+
+			readFrame, isSelfContained, err := readUncompressedFrame(bytes.NewReader(frame))
+
+			if tt.expectedErr != "" {
+				require.Error(t, err)
+				require.Contains(t, err.Error(), tt.expectedErr)
+			} else {
+				require.NoError(t, err)
+				assert.True(t, isSelfContained)
+				assert.Equal(t, framer.buf, readFrame)
+			}
+		})
+	}
+}
+
+func Test_readCompressedFrame(t *testing.T) {
+	tests := []struct {
+		name string
+		// modifyFrameFn is useful for simulating frame data invalidation
+		modifyFrameFn func([]byte) []byte
+		compressor    testMockedCompressor
+
+		// expectedErrorMsg is an error message that should be returned by Error() method.
+		// We need this to understand which of fmt.Errorf() is returned
+		expectedErrorMsg string
+	}{
+		{
+			name: "header crc24 mismatch",
+			modifyFrameFn: func(frame []byte) []byte {
+				// simulating some crc invalidation
+				frame[0] = 255
+				return frame
+			},
+			expectedErrorMsg: "gocql: crc24 mismatch in frame header",
+		},
+		{
+			name: "body crc32 mismatch",
+			modifyFrameFn: func(frame []byte) []byte {
+				// simulating body crc32 mismatch
+				frame[len(frame)-1] = 255
+				return frame
+			},
+			expectedErrorMsg: "gocql: crc32 mismatch in payload",
+		},
+		{
+			name: "invalid frame length",
+			modifyFrameFn: func(frame []byte) []byte {
+				// simulating body length invalidation
+				return frame[:12]
+			},
+			expectedErrorMsg: "gocql: failed to read compressed frame payload",
+		},
+		{
+			name: "cannot read body checksum",
+			modifyFrameFn: func(frame []byte) []byte {
+				// simulating body length invalidation
+				return frame[:len(frame)-4]
+			},
+			expectedErrorMsg: "gocql: failed to read payload crc32",
+		},
+		{
+			name:          "failed to encode payload",
+			modifyFrameFn: nil,
+			compressor: testMockedCompressor{
+				expectedError: errors.New("failed to encode payload"),
+			},
+			expectedErrorMsg: "failed to encode payload",
+		},
+		{
+			name:          "failed to decode payload",
+			modifyFrameFn: nil,
+			compressor: testMockedCompressor{
+				expectedError: errors.New("failed to decode payload"),
+			},
+			expectedErrorMsg: "failed to decode payload",
+		},
+		{
+			name:          "length mismatch after decoding",
+			modifyFrameFn: nil,
+			compressor: testMockedCompressor{
+				invalidateDecodedDataLength: true,
+			},
+			expectedErrorMsg: "gocql: length mismatch after payload decoding",
+		},
+		{
+			name:             "success",
+			modifyFrameFn:    nil,
+			expectedErrorMsg: "",
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			framer := newFramer(nil, protoVersion5)
+			req := writeQueryFrame{
+				statement: "SELECT * FROM system.local",
+				params: queryParams{
+					consistency: Quorum,
+					keyspace:    "gocql_test",
+				},
+			}
+
+			err := req.buildFrame(framer, 128)
+			require.NoError(t, err)
+
+			frame, err := newCompressedFrame(framer.buf, true, testMockedCompressor{})
+			require.NoError(t, err)
+
+			if tt.modifyFrameFn != nil {
+				frame = tt.modifyFrameFn(frame)
+			}
+
+			readFrame, selfContained, err := readCompressedFrame(bytes.NewReader(frame), tt.compressor)
+
+			switch {
+			case tt.expectedErrorMsg != "":
+				require.Error(t, err)
+				require.Contains(t, err.Error(), tt.expectedErrorMsg)
+			case tt.compressor.expectedError != nil:
+				require.ErrorIs(t, err, tt.compressor.expectedError)
+			default:
+				require.NoError(t, err)
+				assert.True(t, selfContained)
+				assert.Equal(t, framer.buf, readFrame)
+			}
+		})
 	}
 }

worryg0d/gocql

frame_test.go
@@ -26,6 +26,10 @@ package gocql
 
 import (
 	"bytes"
+	"encoding/binary"
+	"errors"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
 	"os"
 	"testing"
 )
@@ -125,5 +129,317 @@ func TestFrameReadTooLong(t *testing.T) {
 	}
 	if head.op != opReady {
 		t.Fatalf("expected to get header %v got %v", opReady, head.op)
+	}
+}
+
+func Test_framer_writeExecuteFrame(t *testing.T) {
+	framer := newFramer(nil, protoVersion5)
+	frame := writeExecuteFrame{
+		preparedID:       []byte{1, 2, 3},
+		resultMetadataID: []byte{4, 5, 6},
+		customPayload: map[string][]byte{
+			"key1": []byte("value1"),
+		},
+		params: queryParams{
+			useNowInSeconds:   true,
+			nowInSecondsValue: 123,
+			keyspace:          "test_keyspace",
+		},
+	}
+
+	err := framer.writeExecuteFrame(123, frame.preparedID, frame.resultMetadataID, &frame.params, &frame.customPayload)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	// skipping header
+	framer.buf = framer.buf[9:]
+
+	assertDeepEqual(t, "customPayload", frame.customPayload, framer.readBytesMap())
+	assertDeepEqual(t, "preparedID", frame.preparedID, framer.readShortBytes())
+	assertDeepEqual(t, "resultMetadataID", frame.resultMetadataID, framer.readShortBytes())
+	assertDeepEqual(t, "constistency", frame.params.consistency, Consistency(framer.readShort()))
+
+	flags := framer.readInt()
+	if flags&int(flagWithNowInSeconds) != int(flagWithNowInSeconds) {
+		t.Fatal("expected flagNowInSeconds to be set, but it is not")
+	}
+
+	if flags&int(flagWithKeyspace) != int(flagWithKeyspace) {
+		t.Fatal("expected flagWithKeyspace to be set, but it is not")
+	}
+
+	assertDeepEqual(t, "keyspace", frame.params.keyspace, framer.readString())
+	assertDeepEqual(t, "useNowInSeconds", frame.params.nowInSecondsValue, framer.readInt())
+}
+
+func Test_framer_writeBatchFrame(t *testing.T) {
+	framer := newFramer(nil, protoVersion5)
+	frame := writeBatchFrame{
+		customPayload: map[string][]byte{
+			"key1": []byte("value1"),
+		},
+		useNowInSeconds:   true,
+		nowInSecondsValue: 123,
+	}
+
+	err := framer.writeBatchFrame(123, &frame, frame.customPayload)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	// skipping header
+	framer.buf = framer.buf[9:]
+
+	assertDeepEqual(t, "customPayload", frame.customPayload, framer.readBytesMap())
+	assertDeepEqual(t, "typ", frame.typ, BatchType(framer.readByte()))
+	assertDeepEqual(t, "len(statements)", len(frame.statements), int(framer.readShort()))
+	assertDeepEqual(t, "consistency", frame.consistency, Consistency(framer.readShort()))
+
+	flags := framer.readInt()
+	if flags&int(flagWithNowInSeconds) != int(flagWithNowInSeconds) {
+		t.Fatal("expected flagNowInSeconds to be set, but it is not")
+	}
+
+	assertDeepEqual(t, "useNowInSeconds", frame.nowInSecondsValue, framer.readInt())
+}
+
+type testMockedCompressor struct {
+	// this is an error its methods should return
+	expectedError error
+
+	// invalidateDecodedDataLength allows to simulate data decoding invalidation
+	invalidateDecodedDataLength bool
+}
+
+func (m testMockedCompressor) Name() string {
+	return "testMockedCompressor"
+}
+
+func (m testMockedCompressor) Encode(data []byte) ([]byte, error) {
+	encoded := make([]byte, len(data)+4)
+	binary.BigEndian.PutUint32(encoded, uint32(len(data)))
+	copy(encoded[4:], data)
+	if m.expectedError != nil {
+		return nil, m.expectedError
+	}
+	return encoded, nil
+}
+
+func (m testMockedCompressor) Decode(data []byte) ([]byte, error) {
+	if m.expectedError != nil {
+		return nil, m.expectedError
+	}
+	return data, nil
+}
+
+func (m testMockedCompressor) DecodeSized(data []byte, size uint32) ([]byte, error) {
+	if m.expectedError != nil {
+		return nil, m.expectedError
+	}
+
+	// simulating invalid size of decoded data
+	if m.invalidateDecodedDataLength {
+		return data[:size-1], nil
+	}
+
+	return data, nil
+}
+
+func Test_readUncompressedFrame(t *testing.T) {
+	tests := []struct {
+		name        string
+		modifyFrame func([]byte) []byte
+		expectedErr string
+	}{
+		{
+			name: "header crc24 mismatch",
+			modifyFrame: func(frame []byte) []byte {
+				// simulating some crc invalidation
+				frame[0] = 255
+				return frame
+			},
+			expectedErr: "gocql: crc24 mismatch in frame header",
+		},
+		{
+			name: "body crc32 mismatch",
+			modifyFrame: func(frame []byte) []byte {
+				// simulating body crc32 mismatch
+				frame[len(frame)-1] = 255
+				return frame
+			},
+			expectedErr: "gocql: payload crc32 mismatch",
+		},
+		{
+			name: "invalid frame length",
+			modifyFrame: func(frame []byte) []byte {
+				// simulating body length invalidation
+				frame = frame[:7]
+				return frame
+			},
+			expectedErr: "gocql: failed to read uncompressed frame payload",
+		},
+		{
+			name: "cannot read body checksum",
+			modifyFrame: func(frame []byte) []byte {
+				// simulating body length invalidation
+				frame = frame[:len(frame)-4]
+				return frame
+			},
+			expectedErr: "gocql: failed to read payload crc32",
+		},
+		{
+			name:        "success",
+			modifyFrame: nil,
+			expectedErr: "",
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			framer := newFramer(nil, protoVersion5)
+			req := writeQueryFrame{
+				statement: "SELECT * FROM system.local",
+				params: queryParams{
+					consistency: Quorum,
+					keyspace:    "gocql_test",
+				},
+			}
+
+			err := req.buildFrame(framer, 128)
+			require.NoError(t, err)
+
+			frame, err := newUncompressedFrame(framer.buf, true)
+			require.NoError(t, err)
+
+			if tt.modifyFrame != nil {
+				frame = tt.modifyFrame(frame)
+			}
+
+			readFrame, isSelfContained, err := readUncompressedFrame(bytes.NewReader(frame))
+
+			if tt.expectedErr != "" {
+				require.Error(t, err)
+				require.Contains(t, err.Error(), tt.expectedErr)
+			} else {
+				require.NoError(t, err)
+				assert.True(t, isSelfContained)
+				assert.Equal(t, framer.buf, readFrame)
+			}
+		})
+	}
+}
+
+func Test_readCompressedFrame(t *testing.T) {
+	tests := []struct {
+		name string
+		// modifyFrameFn is useful for simulating frame data invalidation
+		modifyFrameFn func([]byte) []byte
+		compressor    testMockedCompressor
+
+		// expectedErrorMsg is an error message that should be returned by Error() method.
+		// We need this to understand which of fmt.Errorf() is returned
+		expectedErrorMsg string
+	}{
+		{
+			name: "header crc24 mismatch",
+			modifyFrameFn: func(frame []byte) []byte {
+				// simulating some crc invalidation
+				frame[0] = 255
+				return frame
+			},
+			expectedErrorMsg: "gocql: crc24 mismatch in frame header",
+		},
+		{
+			name: "body crc32 mismatch",
+			modifyFrameFn: func(frame []byte) []byte {
+				// simulating body crc32 mismatch
+				frame[len(frame)-1] = 255
+				return frame
+			},
+			expectedErrorMsg: "gocql: crc32 mismatch in payload",
+		},
+		{
+			name: "invalid frame length",
+			modifyFrameFn: func(frame []byte) []byte {
+				// simulating body length invalidation
+				return frame[:12]
+			},
+			expectedErrorMsg: "gocql: failed to read compressed frame payload",
+		},
+		{
+			name: "cannot read body checksum",
+			modifyFrameFn: func(frame []byte) []byte {
+				// simulating body length invalidation
+				return frame[:len(frame)-4]
+			},
+			expectedErrorMsg: "gocql: failed to read payload crc32",
+		},
+		{
+			name:          "failed to encode payload",
+			modifyFrameFn: nil,
+			compressor: testMockedCompressor{
+				expectedError: errors.New("failed to encode payload"),
+			},
+			expectedErrorMsg: "failed to encode payload",
+		},
+		{
+			name:          "failed to decode payload",
+			modifyFrameFn: nil,
+			compressor: testMockedCompressor{
+				expectedError: errors.New("failed to decode payload"),
+			},
+			expectedErrorMsg: "failed to decode payload",
+		},
+		{
+			name:          "length mismatch after decoding",
+			modifyFrameFn: nil,
+			compressor: testMockedCompressor{
+				invalidateDecodedDataLength: true,
+			},
+			expectedErrorMsg: "gocql: length mismatch after payload decoding",
+		},
+		{
+			name:             "success",
+			modifyFrameFn:    nil,
+			expectedErrorMsg: "",
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			framer := newFramer(nil, protoVersion5)
+			req := writeQueryFrame{
+				statement: "SELECT * FROM system.local",
+				params: queryParams{
+					consistency: Quorum,
+					keyspace:    "gocql_test",
+				},
+			}
+
+			err := req.buildFrame(framer, 128)
+			require.NoError(t, err)
+
+			frame, err := newCompressedFrame(framer.buf, true, testMockedCompressor{})
+			require.NoError(t, err)
+
+			if tt.modifyFrameFn != nil {
+				frame = tt.modifyFrameFn(frame)
+			}
+
+			readFrame, selfContained, err := readCompressedFrame(bytes.NewReader(frame), tt.compressor)
+
+			switch {
+			case tt.expectedErrorMsg != "":
+				require.Error(t, err)
+				require.Contains(t, err.Error(), tt.expectedErrorMsg)
+			case tt.compressor.expectedError != nil:
+				require.ErrorIs(t, err, tt.compressor.expectedError)
+			default:
+				require.NoError(t, err)
+				assert.True(t, selfContained)
+				assert.Equal(t, framer.buf, readFrame)
+			}
+		})
 	}
 }